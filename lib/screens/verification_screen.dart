24389c0545d28d7e9fa4c3662d0033dbad21f1b1
Khalid Hussain <khalidhussaink895@gmail.com>
2025-12-03 23:38:14 +0500

    Update verification_screen.dart

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
@@ -145,6 +146,10 @@ class _VerificationScreenState extends State<VerificationScreen> {
                       ),
                       onChanged: (v) => _onOtpChanged(i, v),
 
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly, // <-- only numbers allowed
+                      ],
+
                     ),
                   );
                 }),
@@ -187,3 +192,5 @@ class _VerificationScreenState extends State<VerificationScreen> {
     );
   }
 }
+
+
